fb9ebc98562f70d421d73d826c9192d882f147fa
Shruti <shrutisablaniya93@gmail.com>
2021-09-16 12:26:37 +0530

    Initial Commit

---
+++ Basic Core Programs/Program1_PercentageOfHeadsAndTails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+     class Program1_PercentageOfHeadsAndTails
+    {
+        public static void PercentageCalculate()
+        {
+            Console.Write("Enter number of flips:  ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+          
+
+            int heads = 0;
+            int tails = 0;
+
+            for(int i =0; i < number; i++)
+            {
+                Random random = new Random();
+                double value = random.NextDouble();
+                if (value < 0.5)
+                {
+                    tails++;
+                }
+                else
+                {
+                    heads++;
+                }
+            }
+            Console.WriteLine("Heads:  "+heads);
+            Console.WriteLine("Tails:  " + tails);
+
+            float percentageHeads = (heads / number) * 100;
+            float percentageTails = (tails / number) * 100;
+
+
+            Console.WriteLine("Heads Percentage Probability:  " + percentageHeads);
+            Console.WriteLine("Tails Percentage Probability:  " + percentageTails);
+        }
+    }
+}
---
+++ Basic Core Programs/Program5_PrimeFactorsOfANumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Program5_PrimeFactorsOfANumber
+    {
+        public static void PrimeFactors()
+        {
+            Console.Write("Enter a number:  ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n\nPrime Factors of {0} are:", number);
+            while (number % 2 == 0)
+            {
+                Console.Write(2 + " ");
+                number = number/2;
+            }
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+               
+                while (number % i == 0)
+                {
+                    Console.Write(i + " ");
+                    number = number / i;
+                }
+            }
+
+            if (number > 2)
+                Console.Write(number);
+        }
+    }
+}
