fa55622decdc4b4a0af59bad382a3b41b9da3a65
Shruti <shrutisablaniya93@gmail.com>
2021-09-16 13:41:56 +0530

    Initial Commit

---
+++ Logical Programs/Program1_FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class Program1_FibonacciSeries
+    {
+        public static void FibonacciSeries()
+        {
+            int firstTerm = 0;
+            int secondTerm = 1;
+            Console.Write("Enter number of terms in Fibonacci Series:   ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(firstTerm);
+
+            for(int i=1; i<=num; i++)
+            {
+                Console.Write(" + "+secondTerm);
+                int temp;
+
+                temp = firstTerm;
+                firstTerm = secondTerm;
+                secondTerm = temp + secondTerm;
+                
+            }
+        }
+    }
+}
